64344aaf09043ef63632678de09a47861d20e66d
JGZimmermann <72810396+JGZimmermann@users.noreply.github.com>
2025-10-13 15:27:19 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-php-fpm &
+echo "==> Inicializando ambiente Laravel..."
 
+export PATH="$PATH:/usr/local/bin"
+
+cd /var/www
+
+echo "==> Executando composer update..."
+composer update --no-interaction --prefer-dist --optimize-autoloader
+
+
+# 3️⃣ Rodar migrations
+echo "==> Executando migrations..."
+php artisan migrate --force || true
+
+echo "==> Otimizando app..."
+php artisan optimize || true
+
+echo "==> Iniciando PHP-FPM e Nginx..."
+php-fpm -D
 nginx -g "daemon off;"
